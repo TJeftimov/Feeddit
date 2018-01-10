6f3b2768f7fb723d3de593c4298956d2265bbbca
Teo Jeftimov <tjeftimov@tvz.hr>
2018-01-10 13:33:09 +0100

    Attempt at setting up upvotes/downvotes

--- src/main/java/ag04/feeddit/controllers/MainController.java
+++ src/main/java/ag04/feeddit/controllers/MainController.java
@@ -1,5 +1,6 @@
 package ag04.feeddit.controllers;
 
+import ag04.feeddit.entities.Article;
 import ag04.feeddit.services.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -24,6 +26,16 @@ public class MainController
     {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         ModelAndView modelAndView = new ModelAndView("mainFeed");
+        modelAndView.addObject("articles", articleService.findAllPageable(pageable));
+        return modelAndView;
+    }
+
+    @RequestMapping(value="downvote", method = RequestMethod.POST)
+    public ModelAndView downvote(Article article, Pageable pageable)
+    {
+        ModelAndView modelAndView = new ModelAndView("mainFeed");
+        article.setVotes(article.getVotes() - 1);
+        articleService.saveArticle(article);
         modelAndView.addObject("articles", articleService.findAllPageable(pageable));
         return modelAndView;
     }
